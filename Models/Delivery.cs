78d8f155d25e86d2d4c66545138dffcf9748146b
ionutbiris01 <69477667+ionutbiris01@users.noreply.github.com>
2024-01-05 16:20:36 +0200

    1-delivery initial version

--- Models/Delivery.cs
+++ Models/Delivery.cs
@@ -1,12 +1,20 @@
-﻿namespace TruckManagement.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TruckManagement.Models
 {
     public class Delivery
     {
         public int ID { get; set; }
         //public int DriverID { get; set; }
         //public int statusID { get; set; }
+
+        [Display(Name = "Start Location")]
         public string? StartLocation { get; set; }
+
+        [Display(Name = "End Location")]
         public string? EndLocation { get; set; }
+
+        [Display(Name = "Reference Phone Number")]
         public int PhoneNumber { get; set; }
     }
 }
